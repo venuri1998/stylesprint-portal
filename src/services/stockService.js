817ef601f9fb244c62460b1b4260b71532f8ef76
venuri1998 <venurihettiarachchi@gmail.com>
2024-04-30 22:56:43 +0530

    API updates

--- src/services/stockService.js
+++ src/services/stockService.js
@@ -3,8 +3,6 @@ import axios from 'axios';
 const STOCK_SERVICE_URL = window?.configs?.apiUrl;
 
 export const addStock = async (stockDetails) => {
-  console.log(stockDetails, 'stock details', STOCK_SERVICE_URL);
-
   try {
     const response = await axios.post(`${STOCK_SERVICE_URL}/stocks`, stockDetails, {
       headers: {
@@ -12,12 +10,10 @@ export const addStock = async (stockDetails) => {
       },
     });
 
-    console.log(response, 'this is the response--');
-
-    // if (!response.status === 200) {
-    //   const message = `An error has occurred: ${response.status}`;
-    //   throw new Error(message);
-    // }
+    if (!response.status === 200) {
+      const message = `An error has occurred: ${response.status}`;
+      throw new Error(message);
+    }
 
     return response?.data;
   } catch (error) {
@@ -27,8 +23,12 @@ export const addStock = async (stockDetails) => {
 
 export const getStocks = async (data) => {
   try {
-    const response = await axios.get(`${STOCK_SERVICE_URL}/stocks`);
-    console.log(response, 'this is the response--')
+    const response = await axios.get(`${STOCK_SERVICE_URL}/stocks`,{
+      params: {
+        page: data?.page,
+        limit: data?.limit,
+      }
+    });
     return response?.data;
   } catch (error) {
     console.error('Error fetching stock data', error);
